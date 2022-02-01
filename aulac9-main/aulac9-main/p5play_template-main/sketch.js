30a8c3ef60d17f82985759a7332d18ea5751ac64
gabrielgueirosgomes <98849356+gabrielgueirosgomes@users.noreply.github.com>
2022-02-01 17:45:29 -0300

    Add files via upload

---
+++ aulac9-main/aulac9-main/p5play_template-main/sketch.js
@@ -0,0 +1,41 @@
+var bola
+
+function setup() {
+  createCanvas(1200,400);
+bola=createSprite(600,200,30,30)
+ 
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    bola.position.x = bola.position.x +5;
+
+
+  }
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    bola.position.x = bola.position.x -5;
+  }
+  
+  
+  if (keyIsDown(UP_ARROW)) 
+  {
+    bola.position.y = bola.position.y -5;
+  }
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    bola.position.y = bola.position.y +5;
+
+
+  }
+  
+drawSprites()
+}
+
+
+
+
